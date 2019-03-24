33f9ea911b1709ed9f94b148a575936619905618
Dhanasekaran K <dhanasekaran164@gmail.com>
2019-03-24 17:54:29 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: new Container(
+        margin: new EdgeInsets.all(20.0),
+        child: new Form(
+          child: FormUI(),
+        ),
+      )
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+Widget FormUI(){
+  return new Column(
+    children: <Widget>[
+      new TextFormField(
+        decoration: new InputDecoration(hintText: 'Username'),
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(hintText: 'Email address'),
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(hintText: 'Phone number'),
+      ),
+      new RaisedButton(
+        onPressed: _sendToServer,
+        child: new Text('Submit'),
+      ),
+    ],
+  );
+}
+
+_sendToServer(){
+  print("hai");
+}
